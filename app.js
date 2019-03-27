51181c1dd5886e5bb8f003fe47fc0d7819d1063f
Ryan Jensen <rjensen@sketchdev.io>
2019-03-27 13:18:27 -0500

    'Hello Devops Midwest' ready to go

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello DevOps Midwest!!!'));
+
+if (module.parent === null) {
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+} else {
+  // Export our app for testing purposes
+  exports.app = app;
+}
+
