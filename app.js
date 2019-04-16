5a03efa5af6cd15b1fe3508bb7b1697c38fe1f22
Yabesh Rajbhandari <yabesh@Yabeshs-MacBook-Pro.local>
2019-04-16 14:18:43 -0500

    change output text

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello DevOps Midwest!!!'));
+app.get('/', (req, res) => res.send('Hello DevOps Midwest! 2019!!'));
 
 if (module.parent === null) {
   app.listen(port, () => console.log(`Example app listening on port ${port}!`));
